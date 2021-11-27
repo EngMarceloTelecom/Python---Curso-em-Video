dd72b64f622af32e811e9bc3e9c67056d3aaad9a
Marcelo <90853261+EngMarceloTelecom@users.noreply.github.com>
2021-11-27 15:25:08 -0300

    Update exe78.py
    final

--- Mundo3 - Exe/exe78.py
+++ Mundo3 - Exe/exe78.py
@@ -1,11 +1,22 @@
+'''Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o 
+maior e o menor valor digitado e as 
+suas respectivas posições na lista.'''
+
 num = []
 for i in range(0,5):
     num.append(int(input(f'Digite o {i}ª Numero: ')))
 
-print(num)
+print("="*30)
+print(f'A Minha Lista é {num}')
+print("="*30)
+print(f'O Maior Numero Digitado foi {max(num)} nas posições ',end='')
 
-print(max(num))
+for c, v in enumerate(num):
+    if v == max(num):
+        print(f'{c}...',end='')
 
-print(min(num))
+print(f'\nO Menor Numero Digitado foi {min(num)} nas posições ',end='')
+for c, v in enumerate(num):    
+    if v == min(num):
+        print(f'{c}...',end='')
 
-print(f'O menor Numero é {min(num)} na Posição {enumerate(num)}')
