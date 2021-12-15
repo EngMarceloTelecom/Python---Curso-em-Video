afcd1118822133e8d3536d10c20113a64b02b12e
Marcelo <90853261+EngMarceloTelecom@users.noreply.github.com>
2021-12-15 15:37:42 -0300

    15/12

---
+++ Mundo3 - Exe/exe90.py
@@ -0,0 +1,22 @@
+'''Faça um programa que leia Nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+'''
+aluno = {}
+
+aluno['Nome'] = str(input('Nome: ')).upper().strip()
+aluno['Media'] = float(input(f'Media do {aluno["Nome"]}: '))
+print()
+if aluno['Media'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+    print(f'{aluno["Nome"]} a sua Media é {aluno["Media"]}, e Você Esta APROVADO')
+elif 5 <= aluno['Media'] < 7:
+    aluno['Situação'] = 'Recuperação'
+    print(f'{aluno["Nome"]} a sua Media é {aluno["Media"]}, e Você Esta em RECUPERAÇÃO')
+else:
+    aluno['Situação'] = 'Reprovado'
+    print(f'{aluno["Nome"]} a sua Media é {aluno["Media"]}, e Você Esta REPROVADO')
+print()
+print(aluno)
+print()
+for k,v in aluno.items():
+    print(f'{k} é igual a {v}')
+print('Fim do Programa')
