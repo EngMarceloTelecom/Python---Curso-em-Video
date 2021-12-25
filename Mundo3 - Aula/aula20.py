0ef308c9476cef4edf923932fd1f51ac3a42c1d6
Marcelo <90853261+EngMarceloTelecom@users.noreply.github.com>
2021-12-25 18:15:07 -0300

    25/12/21 18h

---
+++ Mundo3 - Aula/aula20.py
@@ -0,0 +1,25 @@
+def linha():
+    print('='*30) 
+linha()
+print('Marcelo') 
+linha()
+
+def titulo(txt):
+    print(txt)
+    print('='*30)
+#titulo(str(input('Nome: ')))
+titulo('python')
+
+linha()
+def soma(a, b):
+    s = a + b
+    print(s)
+soma(2,4)
+
+linha()
+def contador(*num): #
+    soma = sum(num)
+    print(f'{soma}',end=' ')
+contador(2,3,4,5)
+contador(2,1)
+contador(2,3,1)
---
+++ Mundo3 - Exe/exe96.py
@@ -0,0 +1,8 @@
+def ar(l1,l2):
+    area = l1*l2
+    print('= Area do Terreno =')
+    print(f'A Area {l1} x {l2} é {area}m²')
+
+l1 = float(input('Lado 1 [m]: '))
+l2 = float(input('Lado 2 [m]: '))
+ar(l1, l2)
